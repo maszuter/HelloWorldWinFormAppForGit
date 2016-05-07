6789d384e5755c1c2317617b707884b9e00c5e23
Brian Szuter <BrianSzuter@users.noreply.github.com>
2016-05-06 21:12:40 -0400

    Changing text color
    Changing text color as requested.

--- HelloWorldForGit/Form1.Designer.cs
+++ HelloWorldForGit/Form1.Designer.cs
@@ -38,6 +38,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
             this.label1.Location = new System.Drawing.Point(58, 49);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(146, 29);
